01f0a3deb08609c2162b556a0dcbd0fb97a7a687
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-01-21 07:14:49 -0500

    Adds file storage blueprint to flask app

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,15 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, send_file
 from firebase_admin import credentials, firestore, initialize_app
 
 cur_path = os.path.dirname(os.path.realpath(__file__))
 cred = credentials.Certificate(f"{cur_path}/key.json")
-default_app = initialize_app(cred)
+default_app = initialize_app(cred, {'storageBucket': 'vocal-vision.appspot.com'})
 
 app = Flask(__name__)
 from userAPI import user_api
-
-
+from sessionAPI import session_api
+from fileStorageAPI import fileStorage_api
 
 # create a route to test the API
 @app.route('/test')
@@ -18,14 +18,16 @@ def test():
 
 @app.route('/test/image')
 def test_image():
-    return Flask.send_file(os.curdir + 'test.png')
+    return send_file(cur_path + '/test.png')
 
 @app.route('/test/audio')
 def test_audio():
-    return Flask.send_file(os.curdir + 'test.mp3')
+    return send_file(cur_path + '/test.mp3')
 
 app.register_blueprint(user_api, url_prefix='/api/user')
-app.register_blueprint(user_api, url_prefix='/api/session')
+app.register_blueprint(session_api, url_prefix='/api/session')
+app.register_blueprint(fileStorage_api, url_prefix='/api/fileStorage')
+
 
 
 if __name__ == '__main__':
